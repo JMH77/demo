e63893ccdcb8c27723d188cc1f1c67d3f325422b
jmh <2388575287@qq.com>
2025-12-07 15:07:52 +0800

    solve the problem of disconnect with mysql database

--- Qt_demo/database_basic/databasemanager.cpp
+++ Qt_demo/database_basic/databasemanager.cpp
@@ -1,6 +1,7 @@
 #include "databasemanager.h"
 
 #include <QDebug>
+#include <QSqlError>
 
 databasemanager::databasemanager() {
     m_db = nullptr;
@@ -17,18 +18,19 @@ void databasemanager::initDatabase()
 {
     m_db = new QSqlDatabase(QSqlDatabase::addDatabase("QMYSQL"));
     // QSqlDatabase m_db = QSqlDatabase::addDatabase("QODBC");
-    m_db->setDatabaseName("test1207");
+    m_db->setDatabaseName("MySQL");
     m_db->setHostName("localhost");	//主机地址
-    m_db->setUserName("uroot");			//进入数据库的用户名
+    m_db->setUserName("root");			//进入数据库的用户名
     m_db->setPassword("jaten123");		//进入数据库的密码
     m_db->setPort(3306);				//数据库使用的端口号
     if(!m_db->open()){
+        QString errorstr = m_db->lastError().text();
+        qDebug() << "数据库打开失败，错误信息：" << errorstr;
         qDebug() << "数据库打开失败！";
     }
     else{
         qDebug() << "数据库打开成功！";
     }
-
 }
 
 bool databasemanager::isConnected()
