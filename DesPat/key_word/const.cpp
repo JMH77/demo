d6f63f6c8459260f5aa2f4d00183a9a95f8e268c
jmh <2388575287@qq.com>
2026-01-05 21:53:16 +0800

    补充了 const 的成员函数用法，以及mutable后门用法

--- DesPat/key_word/const.cpp
+++ DesPat/key_word/const.cpp
@@ -1,3 +1,11 @@
+/*
+   todos: 本demo是  const  关键字的一些基本知识
+          但是距离完整的工业级  const  使用还差两个：
+          （1）static const  静态常成员变量
+          （2）const 成员函数与非 const 成员函数的逻辑复用
+          后面有时间可以再看看
+*/
+
 #include <iostream>
 #include <string>
 
@@ -5,7 +13,38 @@ class person {
 public:
   std::string name = "jmh" ;
   int age = 20;
+  const std::string email = "@123";
+  mutable std::string  date = "aabnbcc";
+
+    //3. const成员函数   ---->>       返回类型 函数名() const { ... }
+    void display() const {
+      // (1)不能修改成员变量
+      // age = 10;      //报错  ： 不可修改的左值
+      std::cout << "const 成员函数" << std::endl;
+
+      // (2)尝试修改mutable变量
+      std::cout << "即将修改" << this->date << std::endl;
+      date = "eeffgg";
+      std::cout << "修改完成" <<this->date << std::endl;
+    }
+
+    //4.内部成员函数尝试修改一下内部成员变量
+    void changeInfo() {
+      age = 1;
+      // email = "@test";    //报错
+    }
+
 };
+
+void printPersonInfo(const person& p) {
+  std::cout << "打印对象信息" << p.name << p.age << std::endl;
+  // p.name = "aaa";   报错 去掉const就可以了
+}
+
+//使用指针  这个和const没啥关系纯粹是用一下指针
+void printPersonInfoPoi(const person* p) {
+  std::cout << "打印对象信息" << p->name << p->age << std::endl;
+}
 
 int main() 
 {
@@ -44,6 +83,15 @@ int main()
     std::cout << "尝试切换指针" << std::endl;
     std::cout << "p1  " << p1->name << "and" << p1->age << std::endl;
     std::cout << "p2  " << p2->name << "and" << p2->age << std::endl;
- 
+
+    // 调用一下函数
+    std::cout << "调用打印函数" << std::endl;
+    person p4;
+    printPersonInfo(*p1);
+    printPersonInfo(p4);
+    printPersonInfoPoi(p2);
+    p1->display();
+    p2->display();
+
     return 0;
 }
